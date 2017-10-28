6fee7c3eb2d02c0dc13e1139649f0e6d80495d52
jennifernghinguyen <jennifernghi@yahoo.com>
2017-10-28 14:26:37 -0700

    getAllpost working

--- FireBaseBackEnd/functions/index.js
+++ FireBaseBackEnd/functions/index.js
@@ -6,3 +6,26 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.getAllPosts = functions.https.onRequest((req, res) => {
+    var postsRef = admin.database().ref('post');
+    postsRef.once('value').then(function(snap) {
+      res.status(200).json( snap.val()).end();
+    });
+});
+
+exports.getAllPlaces = functions.https.onRequest((req, res) => {
+    
+    var userID = req.url.substring(req.url.lastIndexOf('/')+1);
+    var postsRef = admin.database().ref('/places/{userID}');
+    
+    console.log("url " + url);
+    postsRef.once('value').then(function(snap) {
+        var places = snap.val();
+       // console.log("places" + req.params.userID);
+      res.status(200).json( places).end();
+    });
+});
